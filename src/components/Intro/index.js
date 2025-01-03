94b7e0512fd668e7ff96e0518011eaec747cc9d1
anhduc111 <154323330+anhduc111@users.noreply.github.com>
2025-01-03 23:28:09 +0700

    03/01/2025

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -2,16 +2,31 @@ import React,{useEffect, useRef, useState} from 'react'
 import styles from './Intro.module.scss';
 import classNames from 'classnames/bind';
 import gsap from 'gsap'
+import {useNavigate} from 'react-router-dom'
 
 const cx = classNames.bind(styles);
 
 
 function Intro() {
+    const navigate = useNavigate()
     const buttonRef = useRef([]);
     const imageRef = useRef([]);
-    const listImg = ['./ctthang1_resize__840x320.png','./laprap1224_laprap1124_slide_840x320.png','./maytinh1224_Slide_840x320.png','./trangpatnership_840x320.png']
+    const listImg = [{
+        url:'./ctthang1_resize__840x320.png',
+        link: 'https://www.fahasa.com/sale?fhs_campaign=homepageslider1'
+        },{
+            url:'./laprap1224_laprap1124_slide_840x320.png',
+            link: 'https://www.fahasa.com/uu-dai-ngap-tran?fhs_campaign=homepageslider2'
+        },{
+            url:'./maytinh1224_Slide_840x320.png',
+            link: 'https://www.fahasa.com/lich-nam-moi?fhs_campaign=homepageslider3'
+        },{
+            url:'./trangpatnership_840x320.png',
+            link:'https://www.fahasa.com/trang-bach-hoa?fhs_campaign=homepageslider4'
+        }]
+    const [adjust,setAdjust] = useState(1);
+    const timerRef = useRef(null);
     const [indexImg,setIndexImg] = useState({
-        prev:listImg.length -1,
         current:0,
         next:1,
     });
@@ -27,47 +42,56 @@ function Intro() {
             duration:0.5
         })
     }
-    useEffect(()=>{
-        gsap.timeline().to(imageRef.current[1],{
-            repeat:-1,
-            repeatDelay:2,
-            xPercent:-100,
-            duration:1
-        },1.8).to(imageRef.current[2],{
-            repeat:-1,
-            repeatDelay:2,
-            xPercent:-100,
-            duration:1
-        },1.8);
-        gsap.to({},{
-            repeat:-1,
-            repeatDelay:1,
-            duration:2,
-            onRepeat:()=>{
-                const index = {
-                    prev:indexImg.current,
-                    current:indexImg.next,
-                    next: (indexImg.next+1) % listImg.length,
-                }
-                console.log(index)
-                setIndexImg(index);
-            }
-        },0)
-    })
+        useEffect(() => {
+                imageRef.current = document.querySelectorAll('.imgIntro')
+                gsap.killTweensOf(imageRef.current);
+                gsap.timeline().fromTo(imageRef.current[0],{
+                    xPercent:0
+                },{
+                    xPercent:adjust * -100,
+                    duration:1
+                },0).fromTo(imageRef.current[1],{
+                    xPercent:adjust * 100 -100
+                },{
+                    xPercent:-100,
+                    duration:1
+                },0)
+                nextImg(2000,1);
+        }, [indexImg]);
+      function nextImg(time,adj){
+        setAdjust(adj)  
+        if (timerRef.current) {
+            clearTimeout(timerRef.current);
+        }
+        timerRef.current = setTimeout(() => {
+          if(adj === 1){
+            console.log(11)
+            setIndexImg((prev) => ({
+                current: prev.next,
+                next: (prev.next+1)%listImg.length
+            }));
+          }else{
+            console.log(22)
+            setIndexImg((prev) => ({
+                current:prev.next,
+                next: prev.current
+            }));
+          }
+        }, time);
+      }
   return (
     <div id={cx('intro')}>
         <div id={cx('firtIntro')}>
             <div id={cx('mainFirtIntro')}>
-                <img ref={(el)=>imageRef.current[0] = el} alt='' src={listImg[indexImg.prev]} />
-                <img ref={(el)=>imageRef.current[1] = el} alt='' src={listImg[indexImg.current]} />
-                <img ref={(el)=>imageRef.current[2] = el} alt='' src={listImg[indexImg.next]} />
+                <img className='imgIntro' alt='' src={listImg[indexImg.current].url} />
+                <img onClick={()=>  window.location.href =listImg[indexImg.next].link}  className='imgIntro' alt='' src={listImg[indexImg.next].url} />
             </div>
             <div id={cx('secondFirtIntro')}>
             <img alt='' src='./thanhtoankhongtienmat_392x156.png' />
             <img alt='' src='./ShopeePay392x156.png'/>
             </div>
-            <button onMouseEnter={hanlderMouse} onMouseLeave={mouseOut} ref={(el)=>buttonRef.current[0] = el}> &lt; </button>
-            <button onMouseEnter={hanlderMouse} onMouseLeave={mouseOut} ref={(el)=>buttonRef.current[1] = el} > &gt; </button>
+            <button onClick={()=>nextImg(0,-1)} onMouseEnter={hanlderMouse} onMouseLeave={mouseOut} ref={(el)=>buttonRef.current[0] = el}> &lt; </button>
+            <button onClick={()=>nextImg(0,1)} onMouseEnter={hanlderMouse} onMouseLeave={mouseOut} ref={(el)=>buttonRef.current[1] = el} > &gt; </button>
         </div>
         <div id={cx('secondIntro')}>    
             <img alt='' src='./LDPCTT1_flashsale_resize_310x210.png' />
